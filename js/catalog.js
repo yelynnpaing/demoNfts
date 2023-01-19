40c9ef3c79810d81e05cb2ec4a7511169660c6d2
yelynnpaing <95997370+yelynnpaing@users.noreply.github.com>
2023-01-19 19:19:50 +0630

    first

---
+++ js/catalog.js
@@ -0,0 +1,85 @@
+let navBar = document.querySelector(".navBar");
+let showFilter  = document.getElementById("show-filter")
+let filterDetail = document.querySelector(".filter-detail");
+let searchText = document.querySelector("#searchText");
+let catalogSearch = document.querySelector("#catalogSearch");
+let nftItems = document.getElementById("nftItems");
+let nftJourney = document.getElementById("nft-journey");
+let catalogItems = document.getElementById("catalog-items");
+let catalogSearchText = document.querySelector("#catalogSearchText");
+let scrollToTop = document.querySelector(".scrollToTop");
+
+let tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+let tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// catalog.addEventListener("mouseenter", function (){
+//     console.log ("show items")
+//     catalogItems.classList.toggle("d-none")
+// })
+// catalog.addEventListener("mouseout", function (){
+//     console.log ("hide items")
+//     catalogItems.classList.toggle("d-none")
+// })
+
+catalogSearch.addEventListener("click",function(){
+    // console.log ("show result")
+    searchResult.classList.toggle("d-none");
+    searchResult.classList.add("mb-3")
+    searchResult.classList.toggle("animate__fadeInDown")
+})
+
+//search result typing
+let typed1 = new Typed(searchText, {
+    strings: ['what do you need ?', 'Find now'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    startDelay:1000,
+    backDelay:500,
+    fadeOut:false,
+    cursorChar:'_',
+    attr: 'placeholder',
+    bindInputFocusEvents: true,
+    loop: true
+});
+
+//filter
+showFilter.addEventListener("click",function (){
+    console.log("click")
+    filterDetail.classList.toggle("d-none");
+    filterDetail.classList.toggle("animate__backInDown")
+})
+
+//countDownClock 
+let countDownAll = document.querySelectorAll(".count-down");
+function clock (){
+    for (let i=0; i<countDownAll.length; i++){
+        let date1 = new Date();
+        let countDownClock =  `${date1.getHours()}:${date1.getMinutes()}:${date1.getSeconds()}`;
+        countDownAll[i].innerText = countDownClock;
+    }
+}
+setInterval(clock,1000)
+
+//navBar Show
+let waypoint1 = new Waypoint({
+    element: document.getElementById('nftItems'),
+    handler: function(direction) {
+        if(direction === "down"){
+            // console.log("waypoint start");
+            navBar.classList.toggle("shadow");
+            navBar.classList.toggle("animate__fadeInDown");
+            navBar.classList.toggle("py-4");
+            scrollToTop.classList.toggle("d-none");
+            scrollToTop.classList.toggle("animate__fadeInUp")
+        }else if(direction === "up") {
+            // console.log("restart");
+            navBar.classList.toggle("animate__fadeInDown");
+            navBar.classList.toggle("shadow");
+            navBar.classList.toggle("py-4");
+            scrollToTop.classList.toggle("d-none")
+            scrollToTop.classList.toggle("animate__fadeInUp")
+        }
+    },
+    offset: '10%'
+});
+
